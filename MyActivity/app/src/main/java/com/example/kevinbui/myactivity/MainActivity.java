250ff2229411b13d167c2e2edd442d9d4bdbd343
buikevin96 <buikevin96@gmail.com>
2019-03-02 10:21:25 -0500

    Activiy life cycle

--- MyActivity/app/src/main/java/com/example/kevinbui/myactivity/MainActivity.java
+++ MyActivity/app/src/main/java/com/example/kevinbui/myactivity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kevinbui.myactivity;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,25 +11,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "OnCreate Called", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        Toast.makeText(MainActivity.this, "OnStart Called", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
+
+        Toast.makeText(MainActivity.this, "OnPostResume Called", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+
+        Toast.makeText(MainActivity.this, "OnStop Called", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        Toast.makeText(MainActivity.this, "OnDestroy Called", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(MainActivity.this, "OnPause Called", Toast.LENGTH_SHORT).show();
+
     }
 }
