c4c125306d22e1ccb8078ca25cabea3f76e45cb1
buikevin96 <buikevin96@gmail.com>
2019-03-02 11:05:53 -0500

    App to switch from one activity to another

---
+++ ShowActivity/app/src/main/java/com/example/kevinbui/showactivity/FirstActivity.java
@@ -0,0 +1,30 @@
+package com.example.kevinbui.showactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private Button goToSecondButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        goToSecondButton = (Button)findViewById(R.id.showButtonID);
+        goToSecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Goes from first activity to second
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+
+                startActivity(new Intent(FirstActivity.this, SecondActivity.class));
+            }
+        });
+    }
+}
