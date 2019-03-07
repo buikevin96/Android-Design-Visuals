fd2c3c29eddf8f47e815348ea802d275bc69cfa0
buikevin96 <buikevin96@gmail.com>
2019-03-06 19:46:54 -0500

    Update MainActivity after SQL code

--- DatabaseIntro/app/src/main/java/Data/DatabaseHandler.java
+++ DatabaseIntro/app/src/main/java/Data/DatabaseHandler.java
@@ -8,13 +8,16 @@ import android.database.sqlite.SQLiteDatabaseLockedException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Model.Contact;
 import Utils.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
     // Constructer method to construct helper
-    public DatabaseHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public DatabaseHandler(@Nullable Context context) {
         super(context, Util.DATABASE_NAME, null, Util.DATEBASE_VERSION);
     }
 
@@ -32,7 +35,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-       // Dropping = delete table
+        // Dropping = delete table
         db.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
 
         // Create table again
@@ -44,7 +47,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
      */
 
-    public void addContact(Contact contact){
+    public void addContact(Contact contact) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues value = new ContentValues();
@@ -65,13 +68,43 @@ public class DatabaseHandler extends SQLiteOpenHelper {
                         Util.KEY_NAME, Util.KEY_PHONE_NUMBER}, Util.KEY_ID + "=?",
                 new String[]{String.valueOf(id)}, null, null, null, null);
 
-        if (cursor != null) {
+        if (cursor != null)
             cursor.moveToFirst();
 
-            Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
-                    cursor.getString(1), cursor.getString(2));
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
 
-            return contact;
+        return contact;
+    }
+
+    // Get all contacts
+    public List<Contact> getAllContacts() {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        List<Contact> contactList = new ArrayList<>();
+
+        // Select all contacts
+
+        String selectAll = "SELECT * FROM" + Util.TABLE_NAME;
+        Cursor cursor = db.rawQuery(selectAll, null);
+
+        // Loop through our contacts
+
+        if (cursor.moveToFirst()){
+            do {
+                Contact contact = new Contact();
+                contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setPhoneNumber(cursor.getString(2));
+
+                // Add contact object to our contact list
+                contactList.add(contact);
+
+            } while (cursor.moveToNext());
         }
+
+        return contactList;
+
     }
 }
--- DatabaseIntro/app/src/main/java/com/example/kevinbui/dbintro/MainActivity.java
+++ DatabaseIntro/app/src/main/java/com/example/kevinbui/dbintro/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.kevinbui.dbintro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import Data.DatabaseHandler;
+import Model.Contact;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        // Insert contacts
+        Log.d("Insert: ", "Inserting...");
+        db.addContact(new Contact("Paul", "7044975555"));
+        db.addContact(new Contact("John", "4144140000"));
+        db.addContact(new Contact("Rose", "7771112222"));
+        db.addContact(new Contact("Bella", "5551111234"));
+
+        // Read them back
+        Log.d("Reading: ", "Reading all contacts...");
+        List<Contact> contactList = db.getAllContacts();
+
+        for (Contact c: contactList){
+            String log = "ID: " + c.getId()+ " , Name: "+ c.getName() + ", Phone: " +c.getPhoneNumber();
+
+            Log.d("Name: " , log);
+        }
     }
 }
